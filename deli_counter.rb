f5a5a1f537e15b609bb5dfdacf6a2df89c0f794a
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-22 01:35:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,6 @@ def take_a_number(num, name)
   spot = num.index(name)
   num.push(name)
   puts "Welcome, #{name}. You are number #{num.index(name)+1} in line."
-  return name, spot
 end
 
 def now_serving(line)
