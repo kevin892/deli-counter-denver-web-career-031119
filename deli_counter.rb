8131e1ea112210ec2034b35832f55fdf74701dc4
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-22 01:18:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,18 +1,6 @@
 # Write your code here.
 katz_deli = []
 
-def line(katz_deli)
-  if katz_deli.length == 0
-    puts "The line is currently empty."
-  else
-    message = "The line is currently:"
-
-    katz_deli.each do |num, name|
-      message += " #{num}. #{name}"
-    end
-    puts "#{message}"
-  end
-end
 
 
 def take_a_number(num, name)
@@ -24,3 +12,16 @@ end
 
 def now_serving(num, name)
   puts
+
+  def line(katz_deli)
+    if katz_deli.length == 0
+      puts "The line is currently empty."
+    else
+      message = "The line is currently:"
+
+      katz_deli.each do |num, name|
+        message += " #{num}. #{name}"
+      end
+      puts "#{message}"
+    end
+  end
