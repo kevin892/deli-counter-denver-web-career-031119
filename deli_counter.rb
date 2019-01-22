07ec7f47a18fb2cdc4f8d76203de855548364b27
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-22 01:20:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,20 @@
 katz_deli = []
 
 
+def line(array) # this was the one I figured out
+  if array.length >= 1
+    nuarray = []
+    counter = 1
+    array.each do |name|
+      nuarray.push("#{counter}. #{name}")
+      counter += 1
+    end
+    puts "The line is currently: #{nuarray.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
 
 def take_a_number(num, name)
   spot = num.index(name)
@@ -9,19 +23,3 @@ def take_a_number(num, name)
   puts "Welcome, #{name}. You are number #{num.index(name)+1} in line."
   return name, spot
 end
-
-def now_serving(num, name)
-  puts
-
-  def line(katz_deli)
-    if katz_deli.length == 0
-      puts "The line is currently empty."
-    else
-      message = "The line is currently:"
-
-      katz_deli.each do |num, name|
-        message += " #{num}. #{name}"
-      end
-      puts "#{message}"
-    end
-  end
