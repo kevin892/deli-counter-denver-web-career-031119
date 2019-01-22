a284fb8c2add6852b6031049ee663225ecf15d48
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-22 01:06:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 # Write your code here.
 katz_deli = []
 
-def take_a_number(array, name)
+def take_a_number(num, name)
   array.push(name)
-  position = array.index(name)
+  spot = num.index(name)
   puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
   return name, position
 end
